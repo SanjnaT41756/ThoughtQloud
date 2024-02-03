b6b5d2befb8e2f5398b94c0898cc0f48e9d0dc0a
Gayatri Puppala <gayatri.puppala03@gmail.com>
2024-02-03 14:18:57 -0800

    delete reduntant react import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Faces from "./Faces.js"; // Adjust the import path based on your file structure
 
-import React from "react";
 import CloudBackground from "./CloudBackground";
 const App = () => {
     const numberOfClouds = 3; // Specify the number of clouds you want
