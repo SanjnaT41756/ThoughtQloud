bc8eac495e471e1d09591c9bbfe114a30b3fb77c
Gayatri Puppala <gayatri.puppala03@gmail.com>
2024-02-03 16:48:45 -0800

    clean up app.js page

--- src/App.js
+++ src/App.js
@@ -3,28 +3,16 @@ import Faces from "./Faces.js"; // Adjust the import path based on your file str
 import SimplePage from "./SimplePage";
 
 import PageList from "./PageList";
-import MotherHuggingImage from "./images/mother_hugging.jpg";
 
 import CloudBackground from "./CloudBackground";
 const App = () => {
     const numberOfClouds = 3; // Specify the number of clouds you want
-    const pageTitle = "Welcome to the Simple Page";
-    const pageImageUrl = MotherHuggingImage; // Provide the image URL
-    const pageText =
-        "This is some text on the simple page. It will adjust its size based on the size of the page. You can adjust the maximum width, font size, and image size as needed.";
-
     return (
         <div>
             {/*
             <CloudBackground numberOfClouds={numberOfClouds} />
             <Faces />
             */}
-
-            {/* <SimplePage
-                title={pageTitle}
-                imageUrl={pageImageUrl}
-                text={pageText}
-            /> */}
             <PageList />
         </div>
     );
